d98ddc65c568aa0f86c1b6ae9b04c1384702da3d
kanguk <lm0828@naver.com>
2024-01-13 15:44:32 +0900

    feat : 이미지 생성 1차 프롬프팅

--- story/views.py
+++ story/views.py
@@ -54,13 +54,31 @@ def story_list_create(request):
             'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 def generate_image(content):
-    # Construct a prompt for OpenAI
-    prompt = f"Generate an image related to the following story:\n{content}"
-    # Request image generation from OpenAI
-    response = openai.Image.create(prompt=prompt, n=1, size="256x256")
-    # Extract the generated image URL
+
+    messages = []
+    promptmessages = "이야기를 기반으로 dall-e api 기반으로 그림을 생성하는 서비스야. 내가 이야기를 작성하면 너가 장면이나 인물 묘사를 훨씬 구체적으로 영어로 변경해주고 이야기와 구체적 묘사들을 영어로 바꿔서 출력해줘. 그리고 너가 생각하기에 그림의 중요 포인트들을 문장 맨 뒤에 키워드로 추가해줘. 사람과 관련된 키워드가 나오면 인물이 들어가도록 키워드에 추가해줘. 날씨나 분위기에 대한 키워드가 너오면 그것도 추가해줘\n"
+    #키워드는 추가 가능
+    prompt_keword = ""
+    # """"Detailed textures", "High resolution", "Dynamic lighting", "Cinematic composition", "Storytelling elements", "Suspenseful atmosphere", "Immersive environment", "Visual storytelling","Sophisticated Style", "Modern Feel","realistic", "fine work",  """
+
+    story_content = promptmessages+content
+    messages.append({"role": "user", "content": story_content})
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    assistant_response = completion['choices'][0]['message']['content'] + prompt_keword
+
+
+    messages.append({"role": "assistant", "content": assistant_response})
+
+    prompt = assistant_response[0:999] # 프롬프트가 1000자가 최대이므로
+
+    response = openai.Image.create(prompt=prompt, n=1, size="256x256") # gpt에게 받은 프롬프팅을 전달하여 그림생성
     image_url = response["data"][0]["url"]
-    return image_url
+
+    return image_url  # 임시로 이미지 url을 리턴하도록 설정
 
 def s3_upload(image_url):  # 생성한 이미지를 s3에 저장
     # 이미지 다운로드
