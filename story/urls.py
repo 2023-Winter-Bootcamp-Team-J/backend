08740da8727014889f0d82558e092937a1183bf5
Gwanghyeon-k <hsys06207@gmail.com>
2024-01-15 04:31:45 +0900

    refactor: api 이름 정리

--- story/urls.py
+++ story/urls.py
@@ -4,6 +4,6 @@ from .views import story_list_create, story_detail, story_all
 urlpatterns = [
     path('stories/', story_list_create, name='story-list-create'),
     path('stories/<int:story_id>/', story_detail, name='story-detail'),
-    path('storiess/<int:story_id>/', story_all, name='story-all')
+    path('stories/branches/<int:story_id>/', story_all, name='story-all')
 ]
 
