dde5020369cceeb200187024f4d38a2daaeb0992
kanguk <lm0828@naver.com>
2024-01-15 22:37:49 +0900

    feat : 프롬프팅 수정

--- story/views.py
+++ story/views.py
@@ -128,9 +128,21 @@ def generate_image(request): # 재생성을 위해 분리함
     content = request.data.get('content', None)
 
     messages = []
-    promptmessages = "이야기를 기반으로 dall-e api 기반으로 그림을 생성하는 서비스야. 내가 이야기를 작성하면 너가 장면이나 인물 묘사를 훨씬 구체적으로 영어로 변경해주고 이야기와 구체적 묘사들을 영어로 바꿔서 출력해줘. 그리고 너가 생각하기에 그림의 중요 포인트들을 문장 맨 뒤에 키워드로 추가해줘. 사람과 관련된 키워드가 나오면 인물이 들어가도록 키워드에 추가해줘. 날씨나 분위기에 대한 키워드가 너오면 그것도 추가해줘\n"
+    promptmessages = """
+우리는 지금 스토리를 작성하면 이미지를 생성해주는 서비스를 진행중이야
+dalle-api를 사용하여 이미지를 생성하는데, 이미지 요청문을 gpt api를 이용하여 생성하고 , 그 요청문을 dalle에게 전달하는 형식으로 진행중이야.
+
+내가 원하는 형식은 우리가 스토리를 작성하면 이야기 속에서 핵심 키워드 (인물, 장소, 사물, 분위기, 배경 등) 를 파악하여 뒤에 키워드로 붙여줘(예시 형태: charactor: Jone, place: school) 
+이 출력값은 그림 생성 요청문으로 쓰일거니까 그림 생성에 적절한 핵심 키워드들을 출력해줘야해 
+그리고 키워드들을 토대로 그림 생성 요청문을 작성해. 그 요청문을 출력하도록 해
+출력값은 영어로 출력하도록 해
+
+story: 
+
+이제 내가 이야기를 입력할거야 바로 내가 원하는 결과값만 출력하도록 해 앞 뒤 설명 필요없이 그림 생성 요청문만 출력하면 돼 story: 
+"""
     #키워드는 추가 가능
-    prompt_keword = ""
+    prompt_keword =  """ \n image_characteristic: "High-Definition" ,"Epic Landscape", "Clear Outlines" , "Fantasy" or "Adventure" ,"High Level of Detail", ""An old, enchanted mansion interior in the style of a classic Disney animation. The image should have vibrant colors and a magical atmosphere, with exaggerated and whimsical design elements that give it a fairy tale-like quality. There should be intricate details and a sense of wonder, akin to the settings often found in animated musical adventures." """
     # """"Detailed textures", "High resolution", "Dynamic lighting", "Cinematic composition", "Storytelling elements", "Suspenseful atmosphere", "Immersive environment", "Visual storytelling","Sophisticated Style", "Modern Feel","realistic", "fine work",  """
 
     story_content = promptmessages+content
