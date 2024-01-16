7d9401270522e2fa42bad36f0bf4984af333f2ff
kanguk <lm0828@naver.com>
2024-01-16 16:12:07 +0900

    feat : 이미지 생성, 조회부분 분리하여 비동기 처리 구현

--- story/views.py
+++ story/views.py
@@ -110,10 +110,11 @@ def story_list_create(request, *args, **kwargs):
         }, status=status.HTTP_201_CREATED)
 
 
+
 @swagger_auto_schema(
     method='post',
-    operation_id='이미지 생성',
-    operation_description='내용에 맞게 ai 이미지를 생성합니다.',
+    operation_id='이미지 생성 요청',
+    operation_description='내용에 맞게 ai 이미지를 생성 요청합니다.',
     tags=['Story'],
     request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT,
@@ -123,53 +124,34 @@ def story_list_create(request, *args, **kwargs):
         required=['content'],
     ),
 )
-@api_view(['POST'])
-def generate_image(request): # 재생성을 위해 분리함
-    content = request.data.get('content', None)
-    task = generate_image_async.delay(content) # 비동기식 task를 여기서 호출
-    result = task.get()
-    return Response(result, status=status.HTTP_200_OK)
- # return Response({'task_id': task.id}, status=status.HTTP_202_ACCEPTED) 비동기처리를 이용하는 좋은방법. task id를 사용하여 결과른 나중에 확인
-#     messages = []
-#     promptmessages = """
-# 우리는 지금 스토리를 작성하면 이미지를 생성해주는 서비스를 진행중이야
-# dalle-api를 사용하여 이미지를 생성하는데, 이미지 요청문을 gpt api를 이용하여 생성하고 , 그 요청문을 dalle에게 전달하는 형식으로 진행중이야.
-#
-# 내가 원하는 형식은 우리가 스토리를 작성하면 이야기 속에서 핵심 키워드 (인물, 장소, 사물, 분위기, 배경 등) 를 파악하여 뒤에 키워드로 붙여줘(예시 형태: charactor: Jone, place: school)
-# 이 출력값은 그림 생성 요청문으로 쓰일거니까 그림 생성에 적절한 핵심 키워드들을 출력해줘야해
-# 그리고 키워드들을 토대로 그림 생성 요청문을 작성해. 그 요청문을 출력하도록 해
-# 출력값은 영어로 출력하도록 해
-#
-# story:
-#
-# 이제 내가 이야기를 입력할거야 바로 내가 원하는 결과값만 출력하도록 해 앞 뒤 설명 필요없이 그림 생성 요청문만 출력하면 돼 story:
-# """
-#     #키워드는 추가 가능
-#     prompt_keword =  """ \n image_characteristic: "High-Definition" ,"Epic Landscape", "Clear Outlines" , "Fantasy" or "Adventure" ,"High Level of Detail", ""An old, enchanted mansion interior in the style of a classic Disney animation. The image should have vibrant colors and a magical atmosphere, with exaggerated and whimsical design elements that give it a fairy tale-like quality. There should be intricate details and a sense of wonder, akin to the settings often found in animated musical adventures." """
-#     # """"Detailed textures", "High resolution", "Dynamic lighting", "Cinematic composition", "Storytelling elements", "Suspenseful atmosphere", "Immersive environment", "Visual storytelling","Sophisticated Style", "Modern Feel","realistic", "fine work",  """
-#
-#     story_content = promptmessages+content
-#     messages.append({"role": "user", "content": story_content})
-#
-#     completion = openai.ChatCompletion.create(
-#         model="gpt-3.5-turbo",
-#         messages=messages
-#     )
-#     assistant_response = completion['choices'][0]['message']['content'] + prompt_keword
-#
-#     messages.append({"role": "assistant", "content": assistant_response})
-#
-#     prompt = assistant_response[0:999] # 프롬프트가 1000자가 최대이므로
-#
-#     response = openai.Image.create(prompt=prompt, n=1, size="256x256") # gpt에게 받은 프롬프팅을 전달하여 그림생성
-#     image_url = response["data"][0]["url"]
-#
-#     return Response({
-#         'message': '이미지가 생성되었습니다.',
-#         'data': {
-#             'image_url': image_url,
-#         }
-#     }, status=status.HTTP_201_CREATED)
+@swagger_auto_schema(
+    method='get',
+    operation_id='생성된 이미지 조회',
+    operation_description='생성 요청된 이미지를 조회합니다.',
+    tags=['Story'],
+    manual_parameters=[
+        openapi.Parameter('task_id', openapi.IN_QUERY, description="Task ID", type=openapi.TYPE_STRING, required=True)
+    ]
+)
+@api_view(['GET', 'POST'])
+def generate_image(request): # 비동기적 이미지 생성
+    if request.method == 'POST': #이미지 생성 비동기요청
+        content = request.data.get('content', None)
+        task = generate_image_async.delay(content) # 비동기식 task를 여기서 호출
+        return Response({'task_id': task.id}, status=status.HTTP_202_ACCEPTED)
+
+    elif request.method == 'GET': #생성된 이미지 조회
+        task_id = request.query_params.get('task_id')
+        if not task_id:
+            return Response({'error' : 'Task ID required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        task = generate_image_async.AsyncResult(task_id)
+        if task.state == 'SUCCESS':
+            return Response({'status': task.state, 'image_url': task.result})
+        elif task.state == 'FAILURE':
+            return Response({'status': task.state, 'error': str(task.result)}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'status': task.state})
 
 def s3_upload(image_url):  # 생성한 이미지를 s3에 저장
     # 이미지 다운로드
